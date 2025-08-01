cde7c0e99e373a2c3ff791d5ca13b251c956ed24
Michael Machado <machado.m3@icloud.com>
2025-08-01 09:52:42 -0300

    feat: add avante && change theme

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,36 +1,48 @@
 -- lua/plugins/mason.lua
 -- Mason setup for managing LSP servers
 
+-- return {
+--   {
+--     "williamboman/mason.nvim",
+--     build = ":MasonUpdate", -- Automatically update registries
+--     config = function()
+--       require("mason").setup()
+--     end,
+--   },
+--   {
+--     "williamboman/mason-lspconfig.nvim",
+--     dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+--     config = function()
+--       require("mason-lspconfig").setup({
+--         ensure_installed = { "lua_ls", "pyright" }, -- Example LSP servers
+--         automatic_installation = true,
+--       })
+--     end,
+--   },
+--   {
+--     "neovim/nvim-lspconfig", -- Neovim LSP configurations
+--     config = function()
+--       local lspconfig = require("lspconfig")
+--
+--       -- Setup servers installed via Mason
+--       local servers = { "lua_ls", "pyright" }
+--       for _, server in ipairs(servers) do
+--         lspconfig[server].setup({})
+--       end
+--     end,
+--   },
+-- }
+
+
 return {
-  {
-    "williamboman/mason.nvim",
-    version = "v1.8.0",
-    build = ":MasonUpdate", -- Automatically update registries
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright" }, -- Example LSP servers
-        automatic_installation = true,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig", -- Neovim LSP configurations
-    config = function()
-      local lspconfig = require("lspconfig")
-
-      -- Setup servers installed via Mason
-      local servers = { "lua_ls", "pyright" }
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({})
-      end
-    end,
-  },
+    "mason-org/mason.nvim",
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+    }
 }
-
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,14 @@
 -- lua/plugins/theme.lua
 -- Tinted Vim Theme setup
+-- osaka_solarized Theme setup
 
 return {
-  "tinted-theming/tinted-vim", -- Tinted Vim theme plugin
+  -- "tinted-theming/tinted-vim", -- Tinted Vim theme plugin
+  "ioksc/vim-osaka-solarized-theme",
   priority = 1000,             -- Load theme early
   config = function()
-    vim.cmd.colorscheme("base16-horizon-dark") -- Set the desired theme
+    -- vim.cmd.colorscheme("base16-horizon-dark") -- Set the desired theme
+    vim.cmd.colorscheme("osaka_solarized") -- Set the desired theme
   end,
 }
 
